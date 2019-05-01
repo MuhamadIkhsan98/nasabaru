48584a032d11abbaa60a4456ab6a4c76dd8fa487
MuhamadIkhsan98 <48472852+MuhamadIkhsan98@users.noreply.github.com>
2019-05-01 20:21:32 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=6Gnp8VBzsmbhzF5KHUsvssZcOkFvzw2xSVdSpG7r";
         
         $("#loadImage").click(function(){
             $.ajax({
